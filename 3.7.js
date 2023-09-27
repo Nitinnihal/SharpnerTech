ca6b53bc0071f3fff098028b10c3466bc541b597
Nitinnihal <nnjha1999@gmail.com>
2023-09-27 10:31:15 +0530

    Arrays and array method

---
+++ 3.7.js
@@ -0,0 +1,14 @@
+const arr = [
+  "Nitin",
+  24,
+  {
+    person: {
+      name: "gopi",
+      age: 24,
+    },
+  },
+];
+
+for (let a of arr) {
+  console.log(a);
+}
---
+++ KickstartingNode/3.7.js
@@ -0,0 +1,26 @@
+// const arr = [
+//   "Nitin",
+//   24,
+//   {
+//     person: {
+//       name: "gopi ",
+//       age: 24,
+//     },
+//   },
+// ];
+
+// console.log(arr);
+
+const arr = [
+  "Nitin",
+  24,
+  {
+    person: {
+      name: "gopi",
+      age: 24,
+    },
+  },
+];
+
+console.log(arr);
+

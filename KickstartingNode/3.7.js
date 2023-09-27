4e47a24801a67869f6e063c327056dc1071c1e43
Nitinnihal <nnjha1999@gmail.com>
2023-09-27 10:34:33 +0530

    some changes

--- KickstartingNode/3.7.js
+++ KickstartingNode/3.7.js
@@ -1,26 +1,11 @@
-// const arr = [
-//   "Nitin",
-//   24,
-//   {
-//     person: {
-//       name: "gopi ",
-//       age: 24,
-//     },
-//   },
-// ];
-
-// console.log(arr);
-
 const arr = [
   "Nitin",
   24,
   {
     person: {
-      name: "gopi",
+      name: "gopi ",
       age: 24,
     },
   },
 ];
-
 console.log(arr);
-
